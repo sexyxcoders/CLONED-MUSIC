7dbe59e519f716b2283d809dbcec377a7fb95f67
⋏ 𝛅 𝛕 ⋏ <kkapilgupta5084@gmail.com>
2025-10-02 16:28:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotsdbfa2f') # youtube song api key
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots4e1026') # youtube song api key
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------

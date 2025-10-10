eef37c6f0cbd721e1b480bd6a50d03f5b6003746
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-10 20:29:21 +0530

    Update userbot.py

--- Clonify/core/userbot.py
+++ Clonify/core/userbot.py
@@ -11,7 +11,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="PROAss1",
+            name="EviliaAssis1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
